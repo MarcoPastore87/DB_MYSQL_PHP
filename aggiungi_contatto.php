dc9f5a22344854e847d2a1da95b39b977f0334cd
MarcoPastore87 <marcopastore181@gmail.com>
2025-11-13 13:05:05 +0100

    add id to delete link

--- aggiungi_contatto.php
+++ aggiungi_contatto.php
@@ -2,22 +2,23 @@
 
 require 'db.php';
 
-//se il form è stato inviato tramite il metodo POST
+//Se il form è stato inviato tramite il metodo POST
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    //RECUPERO I DATI INSERITI NEL FORM
     $nome = $_POST['nome'];
     $telefono = $_POST['telefono'];
     $email = $_POST['email'];
 
-    //query
+    //QUERY DI INSERIMENTO CONTATTO PASSANDO I VALORI DEL FORM
     $sql = "INSERT INTO contatti( nome, telefono, email ) VALUES('$nome','$telefono','$email')";
 
 
-    //eseguo la query
+    //ESEGUO LA QUERY
     mysqli_query($conn, $sql);
 
-    //rendirizzamento utente alla index post inserimento
+    //Rendirizzamento utente alla index post inserimento
     header("Location: index.php");
 
 }
@@ -40,20 +41,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         <h1>Aggiungi contatto</h1>
 
+
+        <!--FORM DI AGGIUNTA CONTATTO-->
         <form action="" method="POST">
+            Nome : 
+                <input name="nome" type="text" required>
 
+            Telefono : 
+                <input name="telefono" type="text" required>
 
-            Nome : <input name="nome" type="text" required>
-
-            Telefono : <input name="telefono" type="text" required>
-
-            Email : <input name="email" type="text" required>
+            Email : 
+                <input name="email" type="text" required>
 
             <button type="submit">Salva</button>
-
-
-        </form>
         
+        </form>
+            <!--LINK PER TORNARE ALL INDEX-->
             <a href="index.php" class="button">Torna alla lista</a>
             
     </div>
