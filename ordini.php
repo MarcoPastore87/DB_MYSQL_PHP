0ce2e6dc23b731c80072fe90959ea620cf511d1c
MarcoPastore87 <marcopastore181@gmail.com>
2025-11-13 16:09:41 +0100

    ordini

---
+++ ordini.php
@@ -0,0 +1,66 @@
+<?php
+    //importo il file db
+    require 'db.php';
+
+
+    $contatto_id = $_GET['id']; // recupero l id del contatto
+
+    $contatto = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM contatti WHERE id=$contatto_id"));
+
+
+    //salvo in una variabile $ordini i risultati della query
+    $ordini = mysqli_query($conn, "SELECT * FROM ordini WHERE contatto_id=$contatto_id"); // query per prendermi tutta la tabella ordini
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordini per contatto</title>
+    <link rel="stylesheet" href="style.css?v<?= time() ?>">
+</head>
+<body>
+
+
+    <div class="container">
+
+        <h2>Ordini di <?= $contatto['nome'] ?> </h2>
+
+        <a href="aggiungi_ordine.php?id= <?= $contatto_id ?>" class="button">Nuovo Ordine</a>
+
+        <table>
+
+            <tr>
+                <th>Prodotto</th>
+                <th>Quantità</th>
+                <th>Data</th>
+            </tr>
+
+            <?php  while ($row = mysqli_fetch_assoc($ordini)) :  ?>
+
+            <tr>
+
+                <td><?= $row['prodotto'] ?></td>
+                <td><?= $row['quantita'] ?></td>
+                <td><?= $row['data_di_ordine'] ?></td>
+
+            </tr>
+
+
+
+             <?php  endwhile;   ?>
+
+        </table>
+
+        
+
+    </div>
+
+
+    
+</body>
+</html>
